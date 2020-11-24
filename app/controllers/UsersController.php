c3eab3c6bcb45d335c0e0411938f6773b2b1570c
Charly <charly2221@hotmail.fr>
2020-11-24 04:39:51 +0100

    Ajout du code pour la modification et la création

--- app/controllers/UsersController.php
+++ app/controllers/UsersController.php
@@ -6,13 +6,79 @@ class UsersController extends Controller
         $users = Users::get();
         $data = compact('users');
         $view = new View('users-manage');
+        
         $view->render($data);
     }
 
     public function delete($id)
     {
         Users::delete($id);
+        flash("message", "L'utilisateur a bien été supprimé.");
         redirect('/users/manage');
+    }
+
+    public function edit($id)
+    {
+        $user = Users::find($id);
+        $user->grade = switch_grade($user->grade);
+
+        $data = compact('user');
+       
+        $view = new View('edit-users');
+        $view->render($data);
+    }
+
+    public function update($data, $id)
+    {
+        $errors = [];
+
+        if (empty($data['lastname'])) {
+            $errors['lastname'] = "Le nom ne peut pas rester vide.";
+        }
+        if (empty($data['firstname'])) {
+            $errors['firstname'] = "Le prénom ne peut pas rester vide.";
+        }
+        if (empty($data['username'])) {
+            $errors['username'] = "Le nom d'utilisateur ne peut pas rester vide.";
+        }
+        if (empty($data['email'])) {
+            $errors['email'] = "L'email ne peut pas rester vide.";
+        }
+        if (empty($data['grade'])) {
+            $errors['grade'] = "Le rang ne peut pas rester vide.";
+        }
+        if (empty($data['city'])) {
+            $errors['city'] = "La ville ne peut pas rester vide.";
+        }
+        if (empty($data['adress'])) {
+            $errors['adress'] = "L'adresse ne peut pas rester vide.";
+        }
+        if (empty($data['zipcode'])) {
+            $errors['zipcode'] = "Le code postal ne peut pas rester vide.";
+        }
+        if (empty($data['phone'])) {
+            $errors['phone'] = "Le numéro de téléphone ne peut pas rester vide.";
+        }
+        if (empty($data['password'])) {
+            $errors['password'] = "Le mot de passe ne peut pas rester vide.";
+        }
+
+        if (!filter_var($data['email'], FILTER_VALIDATE_EMAIL)) {
+            $errors['emailvalid'] = "L'adresse e-mail saisi n'est pas valide.";
+        }
+        if (empty($errors)) {
+            $data['password'] = password_hash($data['password'], PASSWORD_BCRYPT);
+            Users::update($data, $id);
+
+            flash("message", "L'utilisateur a bien été modifié.");
+            
+            header('location:' .  BASE_URL . '/users/manage');
+        } else {
+            flash('data', $data);
+            flash('errors', $errors);
+            
+            header('location:' .  BASE_URL . "/users/{$id}/edit");
+        }
     }
 
     public function index()
@@ -59,7 +125,7 @@ class UsersController extends Controller
     public function getRank($id)
     {
         $data = Users::find($id);
-        return $data->rank;
+        return $data->grade;
     }
 
     public function can($rank_needed, $rank_user)
@@ -81,7 +147,8 @@ class UsersController extends Controller
         $errors = [];
 
         if ($this->checkfield($data)) {
-            $article = Users::create($data);
+            $data['password'] = password_hash($data['password'], PASSWORD_BCRYPT);
+            $users = Users::create($data);
             flash("message", "L'utilisateur a bien été créé");
             redirect('/users/manage');
         } else {
